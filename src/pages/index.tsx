a7cd94021f15beb6fb7a9a689f9ef9c963c779a3
Gabriel <biel_borel@hotmail.com>
2022-08-17 10:21:37 -0300

    Fixes to make test run

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,35 @@
-/* eslint-disable no-shadow */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { Button, Box } from '@chakra-ui/react';
-import { useEffect, useMemo, useRef } from 'react';
+import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
+
 import { Header } from '../components/Header';
 import { CardList } from '../components/CardList';
 import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Image {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface GetImagesResponse {
+  after: string;
+  data: Image[];
+}
+
 export default function Home(): JSX.Element {
-  const fetchImages = async ({ pageParam = null }) => {
-    const { data } = await api.get('/images', {
+  async function fetchImages({ pageParam = null }): Promise<GetImagesResponse> {
+    const { data } = await api('/api/images', {
       params: {
         after: pageParam,
       },
     });
     return data;
-  };
+  }
 
   const {
     data,
@@ -27,18 +39,17 @@ export default function Home(): JSX.Element {
     fetchNextPage,
     hasNextPage,
   } = useInfiniteQuery('images', fetchImages, {
-    getNextPageParam: lastPage => lastPage.after || null,
+    getNextPageParam: lastPage => lastPage?.after || null,
   });
 
   const formattedData = useMemo(() => {
     const formatted = data?.pages.flatMap(imageData => {
       return imageData.data.flat();
     });
-
     return formatted;
   }, [data]);
 
-  if (isLoading && !Error) {
+  if (isLoading && !isError) {
     return <Loading />;
   }
 
@@ -50,16 +61,11 @@ export default function Home(): JSX.Element {
     <>
       <Header />
 
-      <Box maxW={1120} px={20} mx="auto" my={20}>
+      <Box maxW={1120} px={[10, 15, 20]} mx="auto" my={[10, 15, 20]}>
         <CardList cards={formattedData} />
 
         {hasNextPage && (
-          <Button
-            type="button"
-            mt={6}
-            disabled={isFetchingNextPage}
-            onClick={() => fetchNextPage()}
-          >
+          <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
             {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
           </Button>
         )}
